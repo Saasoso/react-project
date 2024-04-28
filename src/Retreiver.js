8b7c6421c7612926e74312f9e46688a108d078ce
Saasoso <113443360+Saasoso@users.noreply.github.com>
2024-04-28 15:45:01 +0100

    Update Retreiver.js

--- src/Retreiver.js
+++ src/Retreiver.js
@@ -6,7 +6,7 @@ import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
 ///Document here
 const path = "./uploads/suicide.pdf"; 
 const chatModel = new ChatOpenAI({
-  apiKey: "sk-proj-5paUoRLCWJaigWZAVBTVT3BlbkFJrqf0vOec1Xg4PTqzKXx4",
+  apiKey: "OPENAI_API_KEY",
     model: "gpt-3.5-turbo",
     temperature: 0.5
   });
